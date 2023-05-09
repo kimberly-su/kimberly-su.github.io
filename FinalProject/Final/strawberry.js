4f10ece314985246f70914423ca4c17472e4fffc
kimberly-su <123607821+kimberly-su@users.noreply.github.com>
2023-05-08 23:16:24 -0400

    Add files via upload

---
+++ FinalProject/Final/strawberry.js
@@ -0,0 +1,25 @@
+const text = document.querySelector(".strawberrytext");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i=0; i < splitText.length; i++) {
+    text.innerHTML+= "<span>"+ splitText[i] + "</span>"
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === split.Text.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
